2de099cd50d0eb1abe185a02917c8a57e0ec0116
DimonIPymba <nazvanie5486@gmail.com>
2023-04-14 22:41:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab4
+{
+    internal class Program
+    {
+      public static Queue<int> _queue = new Queue<int>();
+
+
+      static void Main(string[] args)
+       {
+         object locker = new object();
+
+            Thread thread1 = new Thread(() =>
+             {
+                 Random rand = new Random();
+                 lock (_queue)
+                 {
+                     Console.WriteLine(Thread.CurrentThread.Name);
+                     for (int i = 0; i < 10; i++)
+                     {
+                         _queue.Enqueue(rand.Next(-100, 101));
+                     }
+                 }
+
+            });
+             thread1.Name = "Thread 1";
+            Thread thread2 = new Thread(() =>
+            {
+              Thread.Sleep(1000);
+              Console.WriteLine(Thread.CurrentThread.Name);
+              foreach (int number in _queue)
+              {
+                 Console.Write($"{number} | ");
+              }
+            });
+            thread2.Name = "Thread 2";
+
+            thread1.Start();
+            thread2.Start();
+
+        }
+    }
+}
